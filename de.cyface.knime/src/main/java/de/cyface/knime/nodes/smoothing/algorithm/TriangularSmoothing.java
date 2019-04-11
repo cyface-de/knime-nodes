3804f52b830e83c2cf457c1db568cf9c593fc533
Klemens Muthmann <klemens.muthmann@cyface.de>
2019-04-11 15:30:09 +0200

    Change package structure

---
+++ de.cyface.knime/src/main/java/de/cyface/knime/nodes/smoothing/algorithm/TriangularSmoothing.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 Cyface GmbH
+ * 
+ * This file is part of the Cyface KNIME Nodes.
+ *
+ * The Cyface KNIME Nodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The Cyface KNIME Nodes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Cyface KNIME Nodes. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cyface.knime.nodes.smoothing.algorithm;
+
+/**
+ * A smoothing algorithm that smoothes a value using the weighted mean of the predecessors and successors.
+ * The used weights decrease with distance from the smoothed value.
+ * 
+ * @author Klemens Muthmann
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public final class TriangularSmoothing implements Algorithm {
+
+	@Override
+	public double smooth(double[] window) {
+		double smoothedValue = 0.0;
+		double denominator = 0.0;
+		for(int i=0; i < window.length; i++) {
+			double weight = (i-(window.length/2)>0) ? Integer.valueOf(window.length-i).doubleValue() : Integer.valueOf(i+1).doubleValue();
+			denominator += weight;
+			smoothedValue += weight * window[i];
+		}
+		return smoothedValue/denominator;
+	}
+
+}
---
+++ de.cyface.knime/src/main/java/de/cyface/knime/nodes/timestamp/InputData.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 Cyface GmbH
+ * 
+ * This file is part of the Cyface KNIME Nodes.
+ *
+ * The Cyface KNIME Nodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The Cyface KNIME Nodes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Cyface KNIME Nodes. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cyface.knime.nodes.timestamp;
+
+import org.knime.core.data.DataTable;
+
+/**
+ * A value object wrapping everything required as input for one execution of this node.
+ * 
+ * @author Klemens Muthmann
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public final class InputData {
+    /**
+     * The first table to align.
+     */
+    private final DataTable firstTable;
+    /**
+     * The second table to align.
+     */
+    private final DataTable secondTable;
+    /**
+     * The name of the column to align from the first table.
+     */
+    private final String firstTimestampColumnName;
+    /**
+     * The name of the column to align from the second table.
+     */
+    private final String secondTimestampColumnName;
+
+    /**
+     * Creates a new completely initialized object of this class.
+     * 
+     * @param firstTable The first table to align.
+     * @param secondTable The second table to align.
+     * @param firstTimestampColumnName The name of the column to align from the first table.
+     * @param secondTimestampColumnName The name of the column to align from the second table.
+     */
+    public InputData(final DataTable firstTable, final DataTable secondTable, final String firstTimestampColumnName,
+            final String secondTimestampColumnName) {
+        this.firstTable = firstTable;
+        this.secondTable = secondTable;
+        this.firstTimestampColumnName = firstTimestampColumnName;
+        this.secondTimestampColumnName = secondTimestampColumnName;
+    }
+
+    /**
+     * @return The first table to align.
+     */
+    public DataTable getFirstTable() {
+        return firstTable;
+    }
+
+    /**
+     * @return The second table to align.
+     */
+    public DataTable getSecondTable() {
+        return secondTable;
+    }
+
+    /**
+     * @return The name of the column to align from the first table.
+     */
+    public String getFirstTimestampColumnName() {
+        return firstTimestampColumnName;
+    }
+
+    /**
+     * @return The name of the column to align from the second table.
+     */
+    public String getSecondTimestampColumnName() {
+        return secondTimestampColumnName;
+    }
+}
